16c9e07c484d2b32e9f5d21b92101cd9b15e698d
gwjang <zmdlgl@gmail.com>
2024-08-06 15:37:19 +0900

    feat: 유레카 서버 기본 세팅

---
+++ src/test/kotlin/msa/eureka/EurekaApplicationTests.kt
@@ -0,0 +1,13 @@
+package msa.eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
